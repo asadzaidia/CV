8dc54d4d0cba743c0621c27ce148e6fc0d2c70d7
azaidia <asadzaidi625@gmail.com>
2021-07-12 20:54:26 +0500

    Added eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        React  app
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      React  app
       </header>
-    </div>
-  );
-}
+  </div>
+);
 
 export default App;

9124e6a327f0980e7f3a7a84aa7960eca18955ac
azaidia <asadzaidi625@gmail.com>
2021-07-12 22:06:34 +0500

    added husky and lint-stagged

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App: React.FC = () => (
   <div className="App">
-    <header className="App-header">
-      CV App
-      </header>
+    <header className="App-header">CV App</header>
   </div>
 );
 
